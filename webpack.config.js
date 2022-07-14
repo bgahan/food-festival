b1c4ec09f6c6d2fff17affd484077224a97e883c
Brittany Gahan <brittanygahan89@gmail.com>
2022-07-14 15:01:54 -0400

    add PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 
 module.exports = {
@@ -16,28 +17,28 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  esModule: false,
-                  name(file) {
-                    return '[path][name].[ext]';
-                  },
-                  publicPath(url) {
-                    return url.replace('../', '/assets/');
-                  }
-                }
-              },
-              {
-                loader: 'image-webpack-loader'
-              }
-            ]
-          }
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return '[path][name].[ext]';
+                            },
+                            publicPath(url) {
+                                return url.replace('../', '/assets/');
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
         ]
-      },
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
@@ -45,6 +46,21 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve("assets/img/icons/icon-512x512.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
         })
     ],
     mode: 'development'
